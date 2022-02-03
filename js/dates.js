35259203481d6619be10761444a2faee1673da57
AnastaSAV <anastasia.personalbox@gmail.com>
2022-02-03 17:59:57 +0200

    added func

--- js/dates.js
+++ js/dates.js
@@ -3,32 +3,49 @@
 // и функция должна вернуть день недели словом в зависимости от языка. Очень круто,
 // если выйдет сделать это ОДНОЙ строкой.
 function getDay(date, lang) {
-  const dayNames = {
-    en: {
-      0: 'Sunday',
-      1: 'Monday',
-      2: 'Tuesday',
-      3: 'Wednesday',
-      4: 'Thursday',
-      5: 'Friday',
-      6: 'Saturday',
-    },
-    ru: {
-      0: 'Воскресенье',
-      1: 'Понедельник',
-      2: 'Вторник',
-      3: 'Среда',
-      4: 'Четверг',
-      5: 'Пятница',
-      6: 'Суббота',
-    },
-  };
+	const dayNames = {
+		en: {
+			0: 'Sunday',
+			1: 'Monday',
+			2: 'Tuesday',
+			3: 'Wednesday',
+			4: 'Thursday',
+			5: 'Friday',
+			6: 'Saturday',
+		},
+		ru: {
+			0: 'Воскресенье',
+			1: 'Понедельник',
+			2: 'Вторник',
+			3: 'Среда',
+			4: 'Четверг',
+			5: 'Пятница',
+			6: 'Суббота',
+		},
+	};
+	let enDays = dayNames.en;
+	let ruDays = dayNames.ru;
+	let arrayEn = [];
+	let arrayRu = [];
+	for (let key in enDays) {
+		arrayEn.push(enDays[key]);
+	}
+	for (let key in ruDays) {
+		arrayRu.push(ruDays[key]);
+	}
+	return lang === 'en' ? arrayEn[date.getDay()] : arrayRu[date.getDay()];
 }
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
-function formatTime(date) {}
+function formatTime(date) {
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	hours = hours < 10 ? '0' + hours : hours;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	return `${hours}:${minutes}`;
+}
 
 /*
 Напишите функцию getLastDayOfMonth(year, month), 
@@ -39,10 +56,13 @@ year – год из четырёх цифр, например, 2012.
 month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
-function getLastDayOfMonth(year, month) {}
+function getLastDayOfMonth(year, month) {
+	let newDate = new Date(year, month + 1, 0);
+	return newDate.getDate();
+}
 
 module.exports = {
-  getDay,
-  formatTime,
-  getLastDayOfMonth,
+	getDay,
+	formatTime,
+	getLastDayOfMonth,
 };
